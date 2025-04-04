0f21110655ee6d65bd98c4531e45dbe832b3440c
cristiangsmiranda <san24063@byui.edu>
2025-04-04 18:16:22 -0300

    streaming guide page added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
+import { resolve } from 'path';
 
-dotenv.config(); // Carregar variáveis do .env
+dotenv.config();
 
 export default defineConfig({
-  root: './streaming-guide', // isso aponta o Vite pro local correto do index.html
-
+  root: './streaming-guide',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'streaming-guide/index.html'),
+        popular: resolve(__dirname, 'streaming-guide/popular.html'),
+        search: resolve(__dirname, 'streaming-guide/search.html'),
+      }
+    }
+  },
+  define: {
+    'process.env.TMDB_API_KEY': JSON.stringify(process.env.TMDB_API_KEY),
+  },
 });
