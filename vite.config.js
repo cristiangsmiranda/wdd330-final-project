ded76c7107f2ddd3d41ae602cdc962ad988cc58e
cristiangsmiranda <san24063@byui.edu>
2025-04-04 01:38:13 -0300

    update added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ dotenv.config(); // Carregar variáveis do .env
 
 export default defineConfig({
   root: './streaming-guide', // isso aponta o Vite pro local correto do index.html
-  define: {
-    'process.env.TMDB_API_KEY': JSON.stringify(process.env.TMDB_API_KEY),
-  },
+
 });
